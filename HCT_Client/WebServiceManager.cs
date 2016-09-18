4fce4458f8dfc84f9561bfaf1d3abf50d710940c
parinya2 <priny16@hotmail.com>
2016-09-18 11:33:49 +0700

    Make WebServiceManager extract Image from XML response

--- HCT_Client/WebServiceManager.cs
+++ HCT_Client/WebServiceManager.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Net;
 using System.IO;
+using System.Drawing;
+using System.Xml;
 
 namespace HCT_Client
 {
@@ -23,14 +25,15 @@ namespace HCT_Client
             soapContent = soapContent.Replace(UtilSOAP.GetSoapParamStr(5), UserProfileManager.GetExamSeq());
             soapContent = soapContent.Replace(UtilSOAP.GetSoapParamStr(6), QuizManager.GetExamCourseCode());
 
-            string responseStr = SendSoapRequestToWebService(soapContent);
-            if (WebServiceResultStatus.isErrorCode(responseStr))
+            byte[] responseBytes = SendSoapRequestToWebService(soapContent);
+            if (WebServiceResultStatus.IsErrorBytesCode(responseBytes))
             {
-                string errorCode = responseStr;
+                string errorCode = WebServiceResultStatus.GetErrorStringFromBytesCode(responseBytes);
                 return errorCode;
             }
             else
-            {                
+            {
+                string responseStr = Encoding.UTF8.GetString(responseBytes);
                 string paperTestNo = ExtractValueInsideXMLTag(responseStr, PAPER_TEST_NUMBER_XML_TAG_INSIDE);
                 if (paperTestNo == null)
                 {
@@ -60,15 +63,16 @@ namespace HCT_Client
             soapContent = soapContent.Replace(UtilSOAP.GetSoapParamStr(5), UserProfileManager.GetExamSeq());
             soapContent = soapContent.Replace(UtilSOAP.GetSoapParamStr(6), todayStr);
 
-            string responseStr = SendSoapRequestToWebService(soapContent);
+            byte[] responseBytes = SendSoapRequestToWebService(soapContent);
            
-            if (WebServiceResultStatus.isErrorCode(responseStr))
+            if (WebServiceResultStatus.IsErrorBytesCode(responseBytes))
             {
-                string errorCode = responseStr;
+                string errorCode = WebServiceResultStatus.GetErrorStringFromBytesCode(responseBytes);
                 return errorCode;
             }
             else
             {
+                string responseStr = Encoding.UTF8.GetString(responseBytes);
                 bool isBusinessError = responseStr.Contains(BUSINESS_ERROR_FAULT);
                 if (isBusinessError)
                 {
@@ -76,7 +80,7 @@ namespace HCT_Client
                 }
                 else
                 {
-                    ExtractQuizFromXMLString(responseStr);
+                    ExtractQuizFromXMLBytes(responseBytes);
                     return WebServiceResultStatus.SUCCESS;
                 }
             }
@@ -137,17 +141,16 @@ namespace HCT_Client
             soapContent = soapContent.Replace(UtilSOAP.GetSoapParamStr(18), choiceCodeParamArray[3]);
             soapContent = soapContent.Replace(UtilSOAP.GetSoapParamStr(19), choiceCodeParamArray[4]);
 
-            Console.WriteLine("XX = "+soapContent);
+            byte[] responseBytes = SendSoapRequestToWebService(soapContent);
 
-            string responseStr = SendSoapRequestToWebService(soapContent);
-
-            if (WebServiceResultStatus.isErrorCode(responseStr))
+            if (WebServiceResultStatus.IsErrorBytesCode(responseBytes))
             {
-                string errorCode = responseStr;
+                string errorCode = WebServiceResultStatus.GetErrorStringFromBytesCode(responseBytes);
                 return errorCode;
             }
             else
             {
+                string responseStr = Encoding.UTF8.GetString(responseBytes);
                 bool isBusinessError = responseStr.Contains(BUSINESS_ERROR_FAULT);
                 if (isBusinessError)
                 {
@@ -161,20 +164,125 @@ namespace HCT_Client
             }
         }
 
-        private static void ExtractQuizFromXMLString(string content)
+        private static Bitmap GetBitmapFromBytes(byte[] imageData)
         {
+            Bitmap bmp = null;
+            try
+            {
+                using (MemoryStream ms = new MemoryStream(imageData))
+                {
+                    bmp = new Bitmap(ms);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("SHIT byteArrayToImage " + e.ToString());
+            }
+            return bmp;
+        }
+
+        public static void ExtractQuizFromXMLBytes(byte[] contentBytes)
+        {
+            byte[] cb = contentBytes;
+            List<byte> tmpByteList = new List<byte>();
+            List<byte> tmpByteListImageID = new List<byte>();
+            List<byte> tmpByteListImageData = new List<byte>();
+            Dictionary<string, byte[]> contentDict = new Dictionary<string, byte[]>();
+            string SOAP_CONTENT_KEY = "SOAP";
+            bool foundContentIDHead = false;
+            bool foundImageHeader = false;
+            string imageID = null;
+            bool isJPEG = false;
+            bool isPNG = false;
+
+            for (int i = 0; i < cb.Length - 8; i++)
+            {
+                if (contentDict.Count == 0)
+                {
+                    if (cb[i] == '-' && cb[i + 1] == '-' && cb[i + 2] == 'M' && cb[i + 3] == 'I' &&
+                        cb[i + 4] == 'M' && cb[i + 5] == 'E' && cb[i + 6] == 'B' && cb[i + 7] == 'o' &&
+                        tmpByteList.Count > 0)
+                    {
+                        contentDict.Add(SOAP_CONTENT_KEY, tmpByteList.ToArray());
+                        tmpByteList.Clear();
+                    }
+                    tmpByteList.Add(cb[i]);
+                }
+                else
+                {
+                    if (imageID == null)
+                    {
+                        if (!foundContentIDHead)
+                        {
+                            if (cb[i - 4] == 'I' && cb[i - 3] == 'D' && cb[i - 2] == ':' && cb[i - 1] == ' ' && cb[i] == '<')
+                            {
+                                foundContentIDHead = true;
+                            }
+                        }
+                        else
+                        {
+                            if (cb[i] == '>')
+                            {
+                                imageID = Encoding.UTF8.GetString(tmpByteListImageID.ToArray());
+                            }
+                            else
+                            {
+                                tmpByteListImageID.Add(cb[i]);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (!foundImageHeader)
+                        {
+                            if (cb[i] == 0xFF && cb[i+1] == 0xD8)
+                            {
+                                isJPEG = true;
+                                tmpByteListImageData.Add(cb[i]);
+                                foundImageHeader = true;
+                            }
+                            else if (cb[i] == 0x89 && cb[i + 1] == 0x50 && cb[i + 2] == 0x4E && cb[i + 3] == 0x47)
+                            {
+                                isPNG = true;
+                                tmpByteListImageData.Add(cb[i]);
+                                foundImageHeader = true;
+                            }
+                        }
+                        else
+                        {
+                            if ((cb[i - 2] == 0xFF && cb[i - 1] == 0xD9 && isJPEG) ||
+                                (cb[i - 8] == 0x49 && cb[i - 7] == 0x45 && cb[i - 6] == 0x4E && cb[i - 5] == 0x44 &&
+                                 cb[i - 4] == 0xAE && cb[i - 3] == 0x42 && cb[i - 2] == 0x60 && cb[i - 1] == 0x82 &&
+                                 isPNG))
+                            {
+                                contentDict.Add(imageID, tmpByteListImageData.ToArray());
+                                tmpByteListImageData.Clear();
+                                tmpByteListImageID.Clear();
+                                foundImageHeader = false;
+                                foundContentIDHead = false;
+                                imageID = null;
+                            }
+                            else
+                            {
+                                tmpByteListImageData.Add(cb[i]);
+                            }                            
+                        }
+                    }                   
+                }
+            }
+
             string RESULT_XML_TAG = "<return>";
             string EEXAM_ANSWER_XML_TAG = "<eexamAnswer>";
             string SOAP_BODY_XML_TAG_INSIDE = "soapenv:Body";
-
-            content = ExtractValueInsideXMLTag(content, SOAP_BODY_XML_TAG_INSIDE);
-            
-            string[] tmpStrArray = content.Split(new string[] {RESULT_XML_TAG}, StringSplitOptions.None);
+            byte[] xmlContentBytes = contentDict[SOAP_CONTENT_KEY];
+            string xmlContent = Encoding.UTF8.GetString(xmlContentBytes);
+            xmlContent = ExtractValueInsideXMLTag(xmlContent, SOAP_BODY_XML_TAG_INSIDE);
+            string[] tmpStrArray = xmlContent.Split(new string[] { RESULT_XML_TAG }, StringSplitOptions.None);
 
             // The First element is not used, so we remove it.
-            List<string> tmpList = new List<string>(tmpStrArray);
-            tmpList.RemoveAt(0);
-            tmpStrArray = tmpList.ToArray();
+            List<string> tmpStrList = new List<string>(tmpStrArray);
+            tmpStrList.RemoveAt(0);
+            tmpStrArray = tmpStrList.ToArray();
 
             // Create array of SingleQuizObject
             List<SingleQuizObject> quizList = new List<SingleQuizObject>();
@@ -183,10 +291,20 @@ namespace HCT_Client
                 string tmpContent = tmpStrArray[i];
                 string quizText = ExtractValueInsideXMLTag(tmpContent, "questDesc");
                 string quizCode = ExtractValueInsideXMLTag(tmpContent, "questCode");
+                string quizImageIDFullStr = ExtractValueInsideXMLTag(tmpContent, "questImage");
+                string quizImageID = ExtractAttachmentIDFromXOPString(quizImageIDFullStr);
 
+                Bitmap quizImage = null;
+                if (quizImageID != null && contentDict.ContainsKey(quizImageID))
+                {
+                    byte[] quizImageBytes = contentDict[quizImageID];
+                    quizImage = GetBitmapFromBytes(quizImageBytes);
+                }
+                
                 SingleQuizObject quizObj = new SingleQuizObject();
                 quizObj.quizText = quizText;
                 quizObj.quizCode = quizCode;
+                quizObj.quizImage = quizImage;
 
                 string[] tmpChoiceArray = tmpContent.Split(new string[] { EEXAM_ANSWER_XML_TAG }, StringSplitOptions.RemoveEmptyEntries);
                 for (int k = 0; k < tmpChoiceArray.Length; k++)
@@ -195,9 +313,19 @@ namespace HCT_Client
                     SingleChoiceObject choiceObj = new SingleChoiceObject();
                     string choiceText = ExtractValueInsideXMLTag(tmpChoiceContent, "choiceDesc");
                     string choiceCode = ExtractValueInsideXMLTag(tmpChoiceContent, "choiceCode");
+                    string choiceImageIDFullStr = ExtractValueInsideXMLTag(tmpContent, "choiceImage");
+                    string choiceImageID = ExtractAttachmentIDFromXOPString(choiceImageIDFullStr);
+
+                    Bitmap choiceImage = null;
+                    if (choiceImageID != null && contentDict.ContainsKey(choiceImageID))
+                    {
+                        byte[] choiceImageBytes = contentDict[choiceImageID];
+                        choiceImage = GetBitmapFromBytes(choiceImageBytes);
+                    }
 
                     choiceObj.choiceText = choiceText;
                     choiceObj.choiceCode = choiceCode;
+                    choiceObj.choiceImage = choiceImage;
 
                     quizObj.choiceObjArray[k] = choiceObj;
                 }
@@ -206,7 +334,26 @@ namespace HCT_Client
                 quizList.Add(quizObj);
             }
 
-            QuizManager.SetQuizArray(quizList.ToArray());
+            QuizManager.SetQuizArray(quizList.ToArray());     
+        }
+
+        private static string ExtractAttachmentIDFromXOPString(string str)
+        {
+            if (str == null || str.Length == 0)
+                return null;
+
+            int idx1 = str.IndexOf("cid:");
+            if (idx1 != -1)
+            {
+                str = str.Substring(idx1 + 4);
+                int idx2 = str.IndexOf("\"");
+                if (idx2 != -1)
+                {
+                    str = str.Substring(0, idx2);
+                    return str;
+                }                
+            }
+            return null;
         }
 
         private static void ExtractExamResultFromXMLString(string content)
@@ -234,7 +381,7 @@ namespace HCT_Client
             return result;            
         }
 
-        public static string SendSoapRequestToWebService(string soapContent)
+        public static byte[] SendSoapRequestToWebService(string soapRequestMessage)
         {
             HttpWebResponse httpResponse = null;
             Stream responseStream = null;
@@ -251,7 +398,7 @@ namespace HCT_Client
             //string soapAction = "http://ws.eexam.dlt.go.th/EExamService/findStudentDetailRequest";
             //httpRequest.Headers.Add(String.Format("SOAPAction: \"{0}\"", soapAction));
 
-            byte[] buffer = Encoding.UTF8.GetBytes(soapContent);
+            byte[] buffer = Encoding.UTF8.GetBytes(soapRequestMessage);
             httpRequest.ContentLength = buffer.Length;
 
             Stream requestStream = httpRequest.GetRequestStream();
@@ -261,18 +408,20 @@ namespace HCT_Client
             try
             {
                 using (httpResponse = (HttpWebResponse)httpRequest.GetResponse())
-                {
-                    responseStream = httpResponse.GetResponseStream();
-                    responseStreamReader = new StreamReader(responseStream);
+                {                    
+                    responseStream = httpResponse.GetResponseStream();                          
 
-                    string result = responseStreamReader.ReadToEnd();
+                    MemoryStream ms = new MemoryStream();
+                    responseStream.CopyTo(ms);
+                    byte[] resultBytes = ms.ToArray();
 
+                    string result = Encoding.UTF8.GetString(resultBytes);
                     if (result.Contains("Internal Error"))
                     {
-                        return WebServiceResultStatus.ERROR_SERVER_INTERNAL;
+                        return new byte[] { WebServiceResultStatus.ERROR_BYTE_SERVER_INTERNAL };
                     }
 
-                    return result;        
+                    return resultBytes;                                               
                 }
             }
             catch (Exception e)
@@ -280,10 +429,10 @@ namespace HCT_Client
                 string errStr = e.ToString();
                 if (errStr.Contains("The operation has timed out"))
                 {
-                    return WebServiceResultStatus.ERROR_HTTP_TIMEOUT;
+                    return new byte[] { WebServiceResultStatus.ERROR_BYTE_HTTP_TIMEOUT };
                 }
 
-                return WebServiceResultStatus.ERROR_99;
+                return new byte[] { WebServiceResultStatus.ERROR_BYTE_99 };
             }
             finally
             {
@@ -296,17 +445,55 @@ namespace HCT_Client
 
     class WebServiceResultStatus
     {
-        public static string SUCCESS = "SUCCESS";
-        public static string ERROR_HTTP_TIMEOUT = "ERROR_SendSoapRequestToWebService_HttpTimeout";
-        public static string ERROR_STUDENT_DETAIL_NOT_FOUND = "ERROR_FindStudentDetailWebService_StudentNotFound";
-        public static string ERROR_SERVER_INTERNAL = "ERROR_ServerInternal";
-        public static string ERROR_CANNOT_LOAD_EEXAM = "ERROR_CannotLoadEExam";
-        public static string ERROR_CANNOT_CHECK_EEXAM_RESULT = "ERROR_CannotCheckEExamResult";
-        public static string ERROR_99 = "ERROR_99_SendSoapRequestToWebService_Throw";
+        public const string SUCCESS = "SUCCESS";
+        public const byte ERROR_BYTE_HTTP_TIMEOUT = 0xFF;
+        public const byte ERROR_BYTE_SERVER_INTERNAL = 0xFD;
+        public const byte ERROR_BYTE_99 = 0xFE;
 
-        public static bool isErrorCode(string code)
+        public const string ERROR_HTTP_TIMEOUT = "ERROR_HttpTimeout";
+        public const string ERROR_SERVER_INTERNAL = "ERROR_ServerInternal";
+        public const string ERROR_99 = "ERROR_99";
+
+        public const string ERROR_STUDENT_DETAIL_NOT_FOUND = "ERROR_FindStudentDetailWebService_StudentNotFound";
+        public const string ERROR_CANNOT_LOAD_EEXAM = "ERROR_CannotLoadEExam";
+        public const string ERROR_CANNOT_CHECK_EEXAM_RESULT = "ERROR_CannotCheckEExamResult";
+        
+
+        public static bool IsErrorCode(string code)
         {
             return code.Contains("ERROR_");
+        }
+
+        public static bool IsErrorBytesCode(byte[] bytesCode)
+        {
+            if (bytesCode.Length == 1)
+            {
+                if (bytesCode[0] == ERROR_BYTE_99 ||
+                    bytesCode[0] == ERROR_BYTE_HTTP_TIMEOUT ||
+                    bytesCode[0] == ERROR_BYTE_SERVER_INTERNAL)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static string GetErrorStringFromBytesCode(byte[] bytesCode)
+        {
+            if (bytesCode.Length == 1)
+            {
+                switch (bytesCode[0])
+                {
+                    case ERROR_BYTE_99:              return ERROR_99;
+                    case ERROR_BYTE_HTTP_TIMEOUT:    return ERROR_HTTP_TIMEOUT;
+                    case ERROR_BYTE_SERVER_INTERNAL: return ERROR_SERVER_INTERNAL;
+                    default: return ERROR_99;
+                }
+            }
+            else
+            {
+                return ERROR_99;
+            }
         }
     }
 }
